2030eadb151590288237d543f9dbfab9cc18a635
AAtran19 <ashleystudy101@gmail.com>
2026-09-08 00:29:55 -0500

    Set up component structure,MUI Library, and Pixaby image fetching in Gallery

---
+++ frontend/src/components/leftBar/leftBar.tsx
@@ -0,0 +1,17 @@
+import "./leftBar.css";
+
+
+const LeftBar = () => {
+  return (
+    <div className='leftBar'>LeftBar
+    <div className='menuIcons'>Menu
+        <a href= "/">
+        <img src=""alt=""></img>
+        </a>
+    </div>
+    </div>
+  );
+};
+
+
+export default LeftBar;
---
+++ frontend/src/components/topBar/topBar.tsx
@@ -0,0 +1,12 @@
+import "./topBar.css";
+import Button from '@mui/material/Button';
+
+const TopBar = () => {
+  return (
+    <div className="topBar">TopBar
+    <Button variant="contained">Test</Button>
+    </div>
+  );
+};
+
+export default TopBar;
